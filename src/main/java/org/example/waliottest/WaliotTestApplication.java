a56603099b3799aaeb0515a13f48a8106d9345ed
windows <windows@gmail.com>
2026-02-12 20:23:27 +0300

    init

---
+++ src/main/java/org/example/waliottest/WaliotTestApplication.java
@@ -0,0 +1,13 @@
+package org.example.waliottest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaliotTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaliotTestApplication.class, args);
+    }
+
+}
